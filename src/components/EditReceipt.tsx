fa3818ea5e061a5abd471d037b5a5ee4de9717d8
yue jia <jiayu@sheridancollege.ca>
2024-04-01 20:17:24 -0400

    test

---
+++ src/components/EditReceipt.tsx
@@ -0,0 +1,186 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+export function EditModal({ data, onSave }) {
+    const [editableData, setEditableData] = useState(data);
+    const [open, setOpen] = useState(true);
+
+    const categories = [{value:"grocery",label:"Grocery"},
+                        {value:"electronics", label:"Electronics"},
+                        {value:"home",label:"Home&Gardon"},
+                        {value:"apparel",label: "Apparel"},
+                        {value:"health",label:"Health&Beauty"},
+                        {value:"others",label:"Others"}];
+
+    const handleChange = (e, index, type) => {
+        // Clone the current data to avoid direct state mutation
+        const newData = { ...editableData };
+
+        if (type === 'items' || type === 'tax' || type === 'total') {
+            // Update the specified item's name or price
+            newData[type][index][e.target.name] = e.target.value;
+            console.log("````````````",e.target.name,e.target.value)
+        } else if (type === 'storeName') {
+            newData[type] = e.target.value;
+        }
+
+        setEditableData(newData);
+    };
+
+    function addItem() {
+        // Clone the current items and add a new item
+        const newItem = { itemName: '', price: '' }; // Adjust based on your needs
+        const newItems = [...editableData.items, newItem];
+        setEditableData({ ...editableData, items: newItems });
+    }
+
+    function deleteItem(index) {
+        // Filter out the item at the specified index
+        const newItems = editableData.items.filter((_, i) => i !== index);
+        setEditableData({ ...editableData, items: newItems });
+    }
+
+
+    const handleClose = () => { setOpen(false); window.location.reload(); }
+    return (
+        <div>
+            <Dialog
+                open={open}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                sx={{backgroundImage: `linear-gradient(to top left, #f8f9fa, #B0C4DE)`}}
+            >
+                <Box sx={{ border: '1px solid black', padding: '20px', margin: '20px', backgroundImage: `linear-gradient(to bottom right, #f8f9fa, #B0C4DE)`}}>
+                    <h2>Edit Data</h2>
+                    {/* Example for items, similarly implement for tax and total */}
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}><h3>Items</h3>
+                        <Box>
+                            <IconButton size="small" style={{ color: '#FFA500' }} onClick={() => { addItem() }}><AddIcon /></IconButton>
+                        </Box>
+                    </Box>
+                    {editableData.items.map((item, index) => (
+                        item ?
+                            <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Box>
+                                    <IconButton size="small" style={{ color: '#FFA500' }} onClick={() => { deleteItem(index) }}><CloseIcon /></IconButton>
+                                </Box>
+                                <Box>
+                                    <Input
+                                        name="itemName"
+                                        value={item.itemName}
+                                        onChange={(e) => handleChange(e, index, 'items')}
+                                    />
+                                    <Input
+                                        name="price"
+                                        value={item.price}
+                                        type="number"
+                                        onChange={(e) => handleChange(e, index, 'items')}
+                                        sx={{width:'30%'}}
+                                    />
+                                    <Input
+                                        name="quantity"
+                                        value={item.quantity}
+                                        type="number"
+                                        inputProps={{ min: 0 }}
+                                        onChange={(e) => handleChange(e, index, 'items')}
+                                        sx={{width:'9%'}}
+                                    />
+                                    <TextField
+                                        id="standard-select-currency"
+                                        name="category"
+                                        select                                        
+                                        // defaultValue="grocery"
+                                        value={item.category}
+                                        variant="standard"
+                                        onChange={(e) => handleChange(e, index, 'items')}
+                                        sx={{ width: 90 }}
+                                        >
+                                        {categories.map((option) => (
+                                            <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                            </MenuItem>
+                                        ))}
+                                    </TextField>
+                                    
+                                </Box>
+                            </Box> : null
+                    ))}
+                    <h3>Others</h3>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Box>
+                            <IconButton size="small" style={{ color: '#FFA500' }} ><StorefrontIcon/></IconButton>
+                        </Box>
+                        <Box>
+                            <Input
+                                name="StoreName"
+                                value={'Store'}
+                                readOnly={true}
+                            />
+                            <Input
+                                name="Store"
+                                value={editableData.storeName}
+                                onChange={(e) => handleChange(e, -1, 'storeName')}
+                            />
+                        </Box>
+                    </Box>
+                    {editableData.tax.map((item, index) => (
+                        item ?
+                            <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Box>
+                                    <IconButton size="small" style={{ color: '#FFA500' }} onClick={() => { deleteItem(index) }}><CloseIcon /></IconButton>
+                                </Box>
+                                <Box>
+
+                                    <Input
+                                        name="itemName"
+                                        value={item.itemName}
+                                        onChange={(e) => handleChange(e, index, 'tax')}
+                                    />
+                                    <Input
+                                        name="price"
+                                        value={item.price}
+                                        type="number"
+                                        onChange={(e) => handleChange(e, index, 'tax')}
+                                    />
+                                </Box>
+                            </Box> : null
+                    ))}
+                    {editableData.total.map((item, index) => (
+                        item ?
+                            <Box key={index} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                                <Box>
+                                    <IconButton size="small" style={{ color: '#FFA500' }} onClick={() => { deleteItem(index) }}><CloseIcon /></IconButton>
+                                </Box>
+                                <Box>
+                                    <Input
+                                        name="itemName"
+                                        value={item.itemName}
+                                        onChange={(e) => handleChange(e, index, 'total')}
+                                    />
+                                    <Input
+                                        name="price"
+                                        value={item.price}
+                                        type="number"
+                                        onChange={(e) => handleChange(e, index, 'total')}
+                                    />
+                                </Box>
+                            </Box> : null
+                    ))}
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={() => onSave(editableData)}>Save Changes</Button>
+                    </Box>
+                </Box>
+            </Dialog>
+        </div>
+    );
+}
--- src/components/ReceitProvide.tsx
+++ src/components/ReceitProvide.tsx
@@ -7,14 +7,16 @@ import IconButton from '@mui/material/IconButton';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ForwardIcon from '@mui/icons-material/Forward';
 import Modal from '@mui/material/Modal';
-
-
+import {EditModal} from './EditReceipt.tsx';
+import CircularProgress from '@mui/material/CircularProgress';
 export default function Receit() {
 
     const [photoOpen, setPhotoOpen] = useState(false);
     const [selectedFile, setSelectedFile] = useState(null);
     const [imageSrc, setImageSrc] = useState(null);
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [data_edit, setData_edit] = useState(null);
+    const [loading, setLoading] = useState(false);
     const styleVideo = {
         display: 'flex',
         flexDirection: 'column',
@@ -47,33 +49,155 @@ export default function Receit() {
         }
         event.target.value = '';
     };
+    
+    function GradientCircularProgress() {
+      return (
+        <React.Fragment>
+          <svg width={0} height={0}>
+            <defs>
+              <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                <stop offset="0%" stopColor="#e01cd5" />
+                <stop offset="100%" stopColor="#1CB5E0" />
+              </linearGradient>
+            </defs>
+          </svg>
+          <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} />
+        </React.Fragment>
+      );
+    }
+    // const handleFileUpload = async () => {
+    //     if (selectedFile) {
+    //         const formData = new FormData();
+    //         formData.append('file', selectedFile);
+    //         
+    //         try {
+    //             const response = await fetch(`http://localhost:3001/api/receipts/upload`, {
+    //                 method: 'POST',
+    //                 body: formData,
+    //                 headers: {
+    //                     'Authorization': `Bearer ${sessionStorage.getItem('authToken')}`,
+    //                   },
+    //                 // Include headers if required by your API
+    //             });
 
-    const handleFileUpload = async () => {
+    //             if (response.ok) {
+    //                 const data = await response.json();
+    //                 console.log('Image recognition result:', data);
+    //                 // Handle the response data
+    //             } else {
+    //                 console.error('Upload failed');
+    //             }
+    //         } catch (error) {
+    //             console.error('Error uploading image', error);
+    //         }
+    //     }
+    // };
+    const handleSaveChanges = async (updatedData) => {
+      // Send updated data to backend
+      console.log(JSON.stringify(updatedData));
+      try {
+        const response = await fetch('YOUR_BACKEND_SAVE_ENDPOINT', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedData),
+        });
+  
+        if (response.ok) {
+          // Handle success response
+          console.log('Data saved successfully');
+        } else {
+          // Handle server error response
+          console.error('Server error');
+        }
+      } catch (error) {
+        // Handle network error
+        console.error('Network error:', error);
+      }
+  
+      setIsModalOpen(false); // Close modal after saving
+    };
+
+
+    async function handleFileUpload() {
         if (selectedFile) {
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-            try {
-                const response = await fetch(`${process.env.REACT_APP_API_URL}/api/receipts/upload`, {
+          setLoading(true);
+          const reader = new FileReader();
+      
+          reader.onload = async (event) => {
+            const img = new Image();
+            img.onload = async () => {
+              // Create a canvas and get its context
+              const canvas = document.createElement('canvas');
+              const ctx = canvas.getContext('2d');
+              canvas.width = img.width;
+              canvas.height = img.height;
+      
+              // Draw the image onto the canvas
+              ctx.drawImage(img, 0, 0);
+      
+              // Apply increaseContrast directly on the canvas
+              increaseContrast(ctx);
+      
+              // Convert the canvas content back to a Blob
+              canvas.toBlob(async (blob) => {
+                // Append the Blob to FormData
+                const formData = new FormData();
+                formData.append('file', blob, 'modified-image.png');
+      
+                // Upload the modified image
+                try {
+                  const response = await fetch(`${process.env.REACT_APP_API_URL}/api/receipts/upload`, {
                     method: 'POST',
                     body: formData,
                     headers: {
-                        'Authorization': `Bearer ${sessionStorage.getItem('authToken')}`,
-                      },
-                    // Include headers if required by your API
-                });
-
-                if (response.ok) {
+                      'Authorization': `Bearer ${sessionStorage.getItem('authToken')}`,
+                      // Content-Type is automatically set for FormData
+                    },
+                  });
+                  
+                  if (response.ok) {
                     const data = await response.json();
                     console.log('Image recognition result:', data);
-                    // Handle the response data
-                } else {
+                    setLoading(false);
+                    setData_edit(data);
+                    setIsModalOpen(true);
+                  } else {
                     console.error('Upload failed');
+                  }
+                } catch (error) {
+                  console.error('Error uploading image', error);
                 }
-            } catch (error) {
-                console.error('Error uploading image', error);
-            }
+              }, 'image/png');
+            };
+            img.src = event.target.result;
+          };
+      
+          reader.readAsDataURL(selectedFile);
         }
-    };
+      }
+      
+      // Increase contrast function
+      function increaseContrast(ctx) {
+          const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+          const data = imageData.data;
+      
+          // Algorithm to increase contrast
+          for (let i = 0; i < data.length; i += 4) {
+              // Modify pixel data; simplistic contrast increase for demonstration
+              data[i] = (data[i] - 128) * 1.1 + 128; // R
+              data[i + 1] = (data[i + 1] - 128) * 1.1 + 128; // G
+              data[i + 2] = (data[i + 2] - 128) * 1.1 + 128; // B
+              // Alpha (data[i + 3]) is unchanged
+          }
+      
+          // Put the modified image data back to the canvas
+          ctx.putImageData(imageData, 0, 0);
+      }
+      
+      
+
     const getWidth = () => window.innerWidth 
     || document.documentElement.clientWidth 
     || document.body.clientWidth;
@@ -107,7 +231,8 @@ export default function Receit() {
             <img src={backgroundImg} alt="Grocery Img" style={ imageStyle } />
             <br /><br />
             {photoOpen && <Camera setPhotoOpen={setPhotoOpen} />}
-
+            {isModalOpen && <EditModal data={data_edit} onSave={handleSaveChanges} />}
+            
             {imageSrc &&
                 <div>
                     <Modal
@@ -145,6 +270,7 @@ export default function Receit() {
                     <Button style={{ borderRadius: '25px', backgroundColor: '#F4A460', padding: 10, color: 'black', width: '150px' }} onClick={() => document.getElementById('image-upload').click()}>Upload Receit</Button>
                 </label>
             </Box>
+            {loading && <GradientCircularProgress />}
         </div>
     );
 }
